f745be26e5d32e8edafc892992ec0f016481fed5
GsmPhadi07 <42779956+GsmPhadi07@users.noreply.github.com>
2023-04-21 00:40:17 +0530

    Update higherOrderFunction.js

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,1 +1,25 @@
 
+// Basically, a function which takes another function as an argument or returns a function is known as a higher order function.
+
+function isOdd(num) {
+    return num%2!=0;
+}
+
+function find($num,callBack) {
+    callBack($num);    
+}
+
+//simple call back
+function find(num,callBack) {
+    return callBack($num);    
+}
+
+
+with array
+function filter(arr,callback) {
+    const filterArr=[];
+    for (let index = 0; index < arr.length; index++) {
+        callback(arr[index]) ? filterArr.push(arr[index]) : null;
+    }
+    return filterArr;
+}
